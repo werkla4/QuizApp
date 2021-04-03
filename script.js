7d74ddf45b6beaa091ef1ca179cf6c7ea782a27d
Klaus Werner <werkla4@web.de>
2021-04-03 11:28:42 +0200

    html 3 question test to end

--- script.js
+++ script.js
@@ -25,38 +25,38 @@ let quizData = {
             'd': convInnerHtmlTxt('<thead><body><tr>'),
             'right_answer': 'b'
         },
-        {
-            'question': 'Choose the correct HTML element for the largest heading:',
-            'a': '<h6>',
-            'b': '<head>',
-            'c': '<heading>',
-            'd': '<h1>  ',
-            'right_answer': 'd'
-        },
-        {
-            'question': 'Choose the correct HTML element for the largest heading:',
-            'a': '<h6>',
-            'b': '<head>',
-            'c': '<heading>',
-            'd': '<h1>  ',
-            'right_answer': 'd'
-        },
-        {
-            'question': 'Choose the correct HTML element for the largest heading:',
-            'a': '<h6>',
-            'b': '<head>',
-            'c': '<heading>',
-            'd': '<h1>  ',
-            'right_answer': 'd'
-        },
-        {
-            'question': 'Choose the correct HTML element for the largest heading:',
-            'a': '<h6>',
-            'b': '<head>',
-            'c': '<heading>',
-            'd': '<h1>  ',
-            'right_answer': 'd'
-        },
+        // {
+        //     'question': 'Choose the correct HTML element for the largest heading:',
+        //     'a': '<h6>',
+        //     'b': '<head>',
+        //     'c': '<heading>',
+        //     'd': '<h1>  ',
+        //     'right_answer': 'd'
+        // },
+        // {
+        //     'question': 'Choose the correct HTML element for the largest heading:',
+        //     'a': '<h6>',
+        //     'b': '<head>',
+        //     'c': '<heading>',
+        //     'd': '<h1>  ',
+        //     'right_answer': 'd'
+        // },
+        // {
+        //     'question': 'Choose the correct HTML element for the largest heading:',
+        //     'a': '<h6>',
+        //     'b': '<head>',
+        //     'c': '<heading>',
+        //     'd': '<h1>  ',
+        //     'right_answer': 'd'
+        // },
+        // {
+        //     'question': 'Choose the correct HTML element for the largest heading:',
+        //     'a': '<h6>',
+        //     'b': '<head>',
+        //     'c': '<heading>',
+        //     'd': '<h1>  ',
+        //     'right_answer': 'd'
+        // }
     ]
 }
 let currentGame;
@@ -68,7 +68,7 @@ function init() {
     startGame(currentGame);
 }
 
-function convInnerHtmlTxt(tag_text){
+function convInnerHtmlTxt(tag_text) {
     let convTxt = tag_text.replace('<', '&lt;').replace('>', '&gt;');
     return convTxt;
 }
@@ -80,7 +80,7 @@ function setWelcomeText(game) {
     document.getElementById('custom-welcome').innerHTML = `The Awesome ${upperText} Quiz`;
 }
 
-function showThisPage(page) {
+function showPageHideOthers(page) {
     // hide all pages
     document.getElementById('welcome-page').classList.add('d-none');
     document.getElementById('question-page').classList.add('d-none');
@@ -89,7 +89,7 @@ function showThisPage(page) {
     document.getElementById(page).classList.remove('d-none');
 }
 
-function setLeftBar(game){   
+function setLeftBar(game) {
     // hide all bars
     document.getElementById('bar-left-html').classList.add('d-none');
     document.getElementById('bar-left-css').classList.add('d-none');
@@ -99,25 +99,34 @@ function setLeftBar(game){
     document.getElementById(`bar-left-${game}`).classList.remove('d-none');
 }
 
-function showPage(page, game) {
+function setGame(game) {
+    currentGame = game;
+}
 
-    showThisPage(page); 
+function setcurrentQuestionIndx(indx) {
+    currentQuestionIndx = indx;
+}
+
+function showPage(page, game) {
+    // show page 
+    showPageHideOthers(page);
+    // create wlcometext
     setWelcomeText(game);
+    // set bar left
     setLeftBar(game);
 }
 
 function startGame(game) {
+    // set game
+    setGame(game);
+    // set first question indx
+    setcurrentQuestionIndx(0);
+    // show page
     showPage('welcome-page', game);
 }
 
-function firstQuestion(){
-    if(currentQuestionIndx == 0){ return true; }
-    else { return false; }
-}
-
-function setCurrentTextInQuestionPage(){
+function setCurrentTextInQuestionPage() {
     let cur_quest_answ = quizData[currentGame][currentQuestionIndx];
-    console.log(cur_quest_answ);
     document.getElementById('question-txt').innerHTML = cur_quest_answ['question'];
     document.getElementById('answer-txt-a').innerHTML = cur_quest_answ['a'];
     document.getElementById('answer-txt-b').innerHTML = cur_quest_answ['b'];
@@ -125,39 +134,132 @@ function setCurrentTextInQuestionPage(){
     document.getElementById('answer-txt-d').innerHTML = cur_quest_answ['d'];
 }
 
-function resetAnswerColors(){
-    // reset all elements
-    document.getElementById('list-item-a').classList.remove('lgi-wrong-answer-color');
-    document.getElementById('list-item-a').classList.remove('lgi-correct-answer-color');
-    document.getElementById('answer-box-a').classList.remove('acb-wrong-color');
-    document.getElementById('answer-box-a').classList.remove('acb-correct-color');
-    
-    document.getElementById('list-item-b').classList.remove('lgi-wrong-answer-color');
-    document.getElementById('list-item-b').classList.remove('lgi-correct-answer-color');
-    document.getElementById('answer-box-b').classList.remove('acb-wrong-color');
-    document.getElementById('answer-box-b').classList.remove('acb-correct-color');
-    
-    document.getElementById('list-item-c').classList.remove('lgi-wrong-answer-color');
-    document.getElementById('list-item-c').classList.remove('lgi-correct-answer-color');
-    document.getElementById('answer-box-c').classList.remove('acb-wrong-color');
-    document.getElementById('answer-box-c').classList.remove('acb-correct-color');
-    
-    document.getElementById('list-item-d').classList.remove('lgi-wrong-answer-color');
-    document.getElementById('list-item-d').classList.remove('lgi-correct-answer-color');
-    document.getElementById('answer-box-d').classList.remove('acb-wrong-color');
-    document.getElementById('answer-box-d').classList.remove('acb-correct-color');
-}
+function setItemColor(char, wrong_correct, add_remove) {
+    // get list item name
+    let li_id = `list-item-${char}`;
+    // get answer box name
+    let ab_id = `answer-box-${char}`;
 
-function updateQuestionPage(){
-    resetAnswerColors();
-    setCurrentTextInQuestionPage();    
-}
-
-function getNextQuestion(){
-    // first call: show question page
-    if(firstQuestion()){
-        showPage('question-page', currentGame);
+    // add
+    if (add_remove == 'add') {
+        if (wrong_correct == 'correct') {
+            document.getElementById(li_id).classList.add('lgi-correct-answer-color');
+            document.getElementById(ab_id).classList.add('acb-correct-color');
+        }
+        if (wrong_correct == 'wrong') {
+            document.getElementById(li_id).classList.add('lgi-wrong-answer-color');
+            document.getElementById(ab_id).classList.add('acb-wrong-color');
+        }
     }
-    // show new question with answers
-    updateQuestionPage();
+    // remove
+    if (add_remove == 'remove') {
+        if (wrong_correct == 'correct') {
+            document.getElementById(li_id).classList.remove('lgi-correct-answer-color');
+            document.getElementById(ab_id).classList.remove('acb-correct-color');
+        }
+        if (wrong_correct == 'wrong') {
+            document.getElementById(li_id).classList.remove('lgi-wrong-answer-color');
+            document.getElementById(ab_id).classList.remove('acb-wrong-color');
+        }
+    }
+}
+
+function resetAnswerColors() {
+    // reset all elements
+    let answer_chars = ['a', 'b', 'c', 'd'];
+    // loop all chars
+    for (let i = 0; i < answer_chars.length; i++) {
+        setItemColor(answer_chars[i], 'correct', 'remove');
+        setItemColor(answer_chars[i], 'wrong', 'remove');
+    }
+}
+
+function showQuestionAnswers() {
+    resetAnswerColors();
+    setCurrentTextInQuestionPage();
+    // make buttons active
+    enableButtons();
+}
+
+function getFirstQuestion() {
+    // open question page
+    showPage('question-page', currentGame);
+    // show new question with answer possibilitys
+    showQuestionAnswers();
+}
+
+function checkAnswer(answer_char) {
+    let solution_char = quizData[currentGame][currentQuestionIndx]['right_answer'];
+    // correct answer
+    if (solution_char == answer_char) {
+        setItemColor(answer_char, 'correct', 'add');
+    }
+    // wrong answer
+    else {
+        // mark wrong answer
+        setItemColor(answer_char, 'wrong', 'add');
+        // show/mark correct answer        
+        setItemColor(solution_char, 'correct', 'add');
+    }
+}
+
+function disableButtons() {
+    // get all elements
+    let answer_chars = ['a', 'b', 'c', 'd'];
+    let li_id;
+    // loop all chars
+    for (let i = 0; i < answer_chars.length; i++) {
+        // get id
+        li_id = `list-item-${answer_chars[i]}`;
+        document.getElementById(li_id).classList.add('disable-Item');
+    }
+}
+
+function enableButtons() {
+    // get all elements
+    let answer_chars = ['a', 'b', 'c', 'd'];
+    let li_id;
+    // loop all chars
+    for (let i = 0; i < answer_chars.length; i++) {
+        // get id
+        li_id = `list-item-${answer_chars[i]}`;
+        document.getElementById(li_id).classList.remove('disable-Item');
+    }
+}
+
+function answer(answer_char) {
+    // check answer and color it
+    checkAnswer(answer_char);
+    // can't click again
+    disableButtons();
+}
+
+function getNextQuestion(next_back) {
+    // next Question
+    if (next_back == 'next') {
+        // increment indx
+        currentQuestionIndx++;
+        // out of range?
+        if (currentQuestionIndx >= quizData[currentGame].length) {
+            // show end page
+            showPageHideOthers('end-page');
+        }
+        else {
+            // show next question
+            showQuestionAnswers();
+        }
+    }
+    // Question back
+    if (next_back == 'back') {
+        // decrement indx
+        currentQuestionIndx--;
+        // out of range, show welcome page
+        if (currentQuestionIndx < 0) {
+            startGame(currentGame);
+        }
+        else {
+            // show next question
+            showQuestionAnswers();
+        }
+    }
 }
